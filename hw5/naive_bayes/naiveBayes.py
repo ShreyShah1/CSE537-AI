0fb77ed54b23a3ac94c80bba5328a2d7f771d956
New <shrey@ubuntu.(none)>
2015-12-11 06:16:08 -0800

    Almost working code with smoothing

--- hw5/naive_bayes/naiveBayes.py
+++ hw5/naive_bayes/naiveBayes.py
@@ -1,10 +1,98 @@
 import argparse
+import math
 import readData
+import sys
+
+NO_OF_FEATURES = 784
+TRAINING_LABELS_PATH = 'traininglabels.txt' 
+TRAINING_IMAGES_PATH = 'trainingimages.txt'
+
+TEST_LABELS_PATH = 'testlabels.txt'
+TEST_IMAGES_PATH = 'testimages.txt'
+
+class NaiveBayes(object):
+	def __init__(self, data, instances):
+                self.trainingData = data
+		self.totalInstances = instances
+		# This will have probability of all the labels.
+		self.labelsProbability = []
+		# This will have probability of features given the class labels.
+		# i.e P(Feature = 1 | Class = 1) 
+		self.featureProbability = [[[0, 0, 0] for x in range(NO_OF_FEATURES)] for y in range(NO_OF_FEATURES)]
+	
+		
+        def calProbability(self, A, B):
+		return float(A + 1)/float(B + 3)
+
+	def getCount(self, num, featureNum, instances):
+		count = 0
+		for instance in instances:
+			if (num == int(instance[featureNum])):
+				count = count + 1
+		return count		
+			
+	def naiveBayes(self):
+		############################################
+		# Calculate Basic Probability for all labels.
+		############################################
+		for idx, labelInstances in enumerate(self.trainingData):
+			self.labelsProbability.append(self.calProbability(len(labelInstances), self.totalInstances))
+			for feature in range(784):
+				for i in range(0, 3):
+					featureProb = self.calProbability(
+						self.getCount(i, feature , labelInstances), 
+						len(labelInstances)
+					)
+					self.featureProbability[idx][feature][i] = featureProb
+
+		#print " Labels " + str(self.labelsProbability)
+		#print " ############ "  + str(self.featureProbability)
+	def predictLabels(self, testingData):
+		predictValuesLabels = []
+		for i in range(0, len(testingData)):
+                	for j in range(0, len(testingData[i])):
+                        	 predictValuesLabels.append((i ,self.predictLabel(testingData[i][j])))
+		return predictValuesLabels
+	
+	def accuracy(self, predictedVals):
+		correct = 0
+		for tup in predictedVals:
+			if (tup[0] == tup[1]):
+				correct += 1
+		return correct * 100.0/len(predictedVals)
+	
+	def predictLabel(self, instance):
+		maxSum = -sys.maxint - 1
+		predictLabel = ''
+		for label in range(0, readData.TOTAL_LABELS):
+			####################################
+			# Calculate naive bayes probablity given a class label
+			# P(X  | class = label)
+			####################################
+			sum = self.labelsProbability[label]
+			for i in range(0, len(instance)):	
+				featureProb = self.featureProbability[label][i][instance[i]]
+				if (featureProb):
+					sum *= featureProb
+			
+			if maxSum < sum:
+				maxSum = sum
+				predictLabel = label			
+
+		return predictLabel
 
 def main(args):
-	readData.readTrainingData()
+	trainingData = readData.readData(TRAINING_LABELS_PATH, TRAINING_IMAGES_PATH)
+	totalInstances = 0
+	for i in range(0, len(trainingData)):
+                for fV in range(0, len(trainingData[i])):
+			totalInstances += 1
 
-
+	nb = NaiveBayes(trainingData, totalInstances)
+	nb.naiveBayes()
+	testingData = readData.readData(TEST_LABELS_PATH, TEST_IMAGES_PATH)
+	predictedVals = nb.predictLabels(testingData)
+	print nb.accuracy(predictedVals)
 if __name__ == "__main__":
         parser = argparse.ArgumentParser(description="HomeWork Five")
         parser.add_argument("--input", type=str)
--- hw5/naive_bayes/readData.py
+++ hw5/naive_bayes/readData.py
@@ -1,40 +1,42 @@
 ################################################
 ## this files read data from training data file
 ###############################################
+import copy
+TOTAL_LABELS = 10
 
-
-def readTrainingData():
+def readData(labels, images):
 	
 	###reading from training label file
 
-	labels = [line.rstrip('\n') for line in open('traininglabels.txt')]
+	labels = [line.rstrip('\n') for line in open(labels)]
 
 	### reading from training images file
 
-	imageData = [line.rstrip('\n') for line in open('trainingimages.txt')]
+	imageData = [line.rstrip('\n') for line in open(images)]
 
-	trainingData = [[0 for x in range(785)] for x in range(len(labels))] 
+	data = [[] for x in range(TOTAL_LABELS)] 
 
-	counter = 1
+	counter = 0
 	row = 0
-
+	tempData = []
 	for i in range(0,len(imageData)):
 
-		if(counter == 1):
-			trainingData[row][0]=labels[row]
-
+		if(counter == 0):
+			tempData = []			
+			
 		for j in range(0,28):
 			if(imageData[i][j] == ' '):
-				trainingData[0][counter] = 0
+				tempData.append(0)
 			if(imageData[i][j] == '#'):
-				trainingData[0][counter] = 1 
+				tempData.append(1) 
+			if(imageData[i][j] == '+'):
+				tempData.append(2)
 			counter = counter + 1
 	
-	 	if(counter == 785):	
-			counter = 1
-			row = row + 1	
-		
-	print 	imageData[6][12]
-	print	"len"+str(len(imageData)) 	
-	print 	trainingData[12][0]
-
+	 	if(counter == 784):	
+			counter = 0
+			classification = int(labels[row])
+			row = row + 1
+			data[classification].append(copy.deepcopy(tempData))
+			
+	return data
